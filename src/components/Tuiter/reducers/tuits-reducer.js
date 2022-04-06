a34525383ee9965fa616fff05f8970fc6e9229d0
mmorehead22 <morehead.m@husky.neu.edu>
2022-04-06 19:16:01 -0400

    clean up

--- src/components/Tuiter/reducers/tuits-reducer.js
+++ src/components/Tuiter/reducers/tuits-reducer.js
@@ -10,21 +10,6 @@ const tuitsReducer =
                 return state.map(
                     tuit => tuit._id === action.tuit._id ?
                         action.tuit : tuit);
-            /*case 'like-tuit':
-                return state.map(tuit => {
-                    if(tuit._id === action.tuit._id) {
-                        if(tuit.liked === true) {
-                            tuit.liked = false;
-                            tuit.stats.likes--;
-                        } else {
-                            tuit.liked = true;
-                            tuit.stats.likes++;
-                        }
-                        return tuit;
-                    } else {
-                        return tuit;
-                    }
-                });*/
             case DELETE_TUIT:
                 return state.filter(
                     tuit => tuit._id !== action.tuit._id);
@@ -33,26 +18,6 @@ const tuitsReducer =
                     ...state,
                     action.newTuit
                 ];
-            /*case 'create-tuit':
-                const newTuit = {
-                    tuit: action.tuit,
-                    _id: (new Date()).getTime() + '',
-                    postedBy: {
-                        "username": "ReactJS"
-                    },
-                    handle: "ReactJS",
-                    logo: "https://i.imgur.com/eRgGBho.png",
-                    avatar: "https://i.imgur.com/eRgGBho.png",
-                    stats: {
-                        retuits: 111,
-                        likes: 222,
-                        comments: 333
-                    }
-                }
-                return [
-                    newTuit,
-                    ...state,
-                ];*/
             default:
                 return state
         }
